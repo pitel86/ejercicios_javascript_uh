de1fea8df28e88fe5ad4d8b4a3e4f455ca3e0d23
pitel86 <pitelsergio@gmail.com>
2021-12-09 18:40:58 +0100

    Iteration#6 Completado

---
+++ upgrade-basic-algorithms/iteracion6/iteracion6.js
@@ -0,0 +1,25 @@
+//1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+for (let index = 0; index < 10; index++) {
+    console.log("Loop1", index);    
+}
+
+/*1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+cuando el resto del numero dividido entre 2 sea 0.*/
+for (let index = 0; index < 10; index++) {
+    if ((index % 2) == 0) {        
+        console.log("Loop2", index);   
+    } 
+}
+
+
+/*1.3 Crea un bucle para conseguir dormir contando ovejas. 
+Este bucle empieza en 0 y termina en 10. 
+Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle 
+y cambia el mensaje en la última vuelta a 'Dormido!'.*/
+for (let index = 0; index <= 10; index++) {
+    if (index != 10) {        
+        console.log("Loop3", "Intentando dormir");   
+    }else{
+        console.log("Loop3", "Dormido!");   
+    }
+}
