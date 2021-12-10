531861ecfb0c10e521234683b6e7d7fbc64addba
pitel86 <pitelsergio@gmail.com>
2021-12-10 18:30:24 +0100

    Iteration#7 completado

---
+++ upgrade-basic-functions /iteracion7/iteracion7.js
@@ -0,0 +1,28 @@
+//Crea una función que reciba por parámetro un array y el valor que desea comprobar
+// que existe dentro de dicho array - comprueba si existe el elemento, en caso que
+// existan nos devuelve un true y la posición de dicho elemento y por la contra un false.
+// Puedes usar este array para probar tu función:
+
+const nameFinder = [
+  "Peter",
+  "Steve",
+  "Tony",
+  "Natasha",
+  "Clint",
+  "Logan",
+  "Xabier",
+  "Bruce",
+  "Peggy",
+  "Jessica",
+  "Marc",
+];
+function finderName(arrayNames, name) {
+  var position = arrayNames.indexOf(name);
+  if (position != -1) {
+    return "El nombre: " + name + " esta en la posicion " + position;
+  } else {
+    return false;
+  }
+}
+
+console.log(finderName(nameFinder, "Peggy"));
