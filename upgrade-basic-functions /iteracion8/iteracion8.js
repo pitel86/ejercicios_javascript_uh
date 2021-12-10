4d74427c13901f52668fedf0ae688baeca4cad88
pitel86 <pitelsergio@gmail.com>
2021-12-10 18:57:24 +0100

    Iteration#8 Completado

---
+++ upgrade-basic-functions /iteracion8/iteracion8.js
@@ -0,0 +1,30 @@
+//Crea una función que nos devuelva el número de veces que se repite cada una de las
+//palabras que lo conforma.  Puedes usar este array para probar tu función:
+
+const counterWords = [
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "upgrade",
+  "code",
+];
+function repeatCounter(param) {
+  let counter = [];
+  for (let i = 0; i < param.length; i++) {
+    
+    if (counter[param[i]] !== undefined) {
+      counter[param[i]]++;
+    } else {
+      counter[param[i]] = 1;
+    }
+  }
+  return counter;
+}
+
+console.log(repeatCounter(counterWords));
