014bd6475f66bbf65bbff954e4f27409c05acdfc
pitel86 <pitelsergio@gmail.com>
2021-12-15 19:25:21 +0100

    Iteration#3 Completada

--- upgrade-es6/iteracion3/iteracion3.js
+++ upgrade-es6/iteracion3/iteracion3.js
@@ -1,19 +1,37 @@
 // 3.1 Dado el siguiente array, crea una copia usando spread operators.
 const pointsList = [32, 54, 21, 64, 75, 43]
 
+const copyList = [...pointsList];
+console.log(copyList);
+
 // 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 
+const copyToy = {...toy}
+console.log(copyToy);
+
 // 3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
 // spread operatos.
-const pointsList = [32, 54, 21, 64, 75, 43];
+const pointsList3 = [32, 54, 21, 64, 75, 43];
 const pointsLis2 = [54,87,99,65,32];
+
+const pointsList4 = [...pointsList3,...pointsLis2];
+console.log(pointsList4);
 
 // 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
 // con spread operators.
-const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toy2 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+
+const newToy = {...toy2,...toyUpdate};
+console.log(newToy);
 
 // 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
 // pero sin editar el array inicial. De nuevo, usando spread operatos.
 const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+const newColors = [...colors];
+newColors.splice(2,1);
+
+console.log(colors);
+console.log(newColors);
