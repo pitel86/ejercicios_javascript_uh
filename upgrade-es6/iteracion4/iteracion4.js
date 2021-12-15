32a2dfeb1676200de71dc0178753a6c455d2f403
pitel86 <pitelsergio@gmail.com>
2021-12-15 19:28:31 +0100

    Iteration#4 Completada

--- upgrade-es6/iteracion4/iteracion4.js
+++ upgrade-es6/iteracion4/iteracion4.js
@@ -7,17 +7,24 @@ const users = [
 	{id:4, name: 'Amanda'}
 ];
 
+const userNames = users.map((user) => user.name);
 
+console.log(userNames);
 
 // 4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
 // de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
 // empiece por 'A'.
-const users = [
-	{id: 1, name: 'Abel'},
-	{id:2, name: 'Julia'},
-	{id:3, name: 'Pedro'},
-	{id:4, name: 'Amanda'}
-];
+// const users = [
+// 	{id: 1, name: 'Abel'},
+// 	{id:2, name: 'Julia'},
+// 	{id:3, name: 'Pedro'},
+// 	{id:4, name: 'Amanda'}
+// ];
+
+const usersName = users.map((user) => user.name[0] === "A" ? "Anacleto": user.name);
+
+console.log(usersName);
+
 
 // 4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
 // de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
@@ -28,3 +35,7 @@ const cities = [
 	{isVisited:true, name: 'Amsterdam'}, 
 	{isVisited:false, name: 'Seul'}
 ];
+
+const citiesNames = cities.map(city => city.isVisited ? city.name + "--Visitada": city.name + "--No Visitada");
+
+console.log(citiesNames);
