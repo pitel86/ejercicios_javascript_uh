ef099eee74abbfc3cd941850575f2503dd88164d
pitel86 <pitelsergio@gmail.com>
2021-12-15 19:21:27 +0100

    Iteration#2 Completado

--- upgrade-es6/iteracion2/iteracion2.js
+++ upgrade-es6/iteracion2/iteracion2.js
@@ -4,11 +4,20 @@
 
 const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
 
+const {gender, year, title} = game;
+console.log(gender);
+console.log(year);
+console.log(title);
+
 // 2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
 // llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
 // imprimelo por consola.
 
 const fruits = ['Banana', 'Strawberry', 'Orange'];
+const [fruit1, fruit2, fruit3] = fruits;
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
 
 // 2.3 En base al siguiente javascript, usa destructuring para crear 2 
 // variables igualandolo a la función e imprimiendolo por consola.
@@ -16,6 +25,9 @@ const fruits = ['Banana', 'Strawberry', 'Orange'];
 const animalFunction = () => {
     return {name: 'Bengal Tiger', race: 'Tiger'}
 };
+const {race, name} = animalFunction();
+console.log(name);
+console.log(race);
 
 // 2.4 En base al siguiente javascript, usa destructuring para crear las 
 // variables name y itv con sus respectivos valores. Posteriormente crea 
@@ -23,3 +35,11 @@ const animalFunction = () => {
 // y comprueba que todo esta bien imprimiendolo.
 
 const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+const {name: carName, itv} = car;
+
+const [year1, year2, year3] = itv;
+
+console.log(carName);
+console.log(year1);
+console.log(year2);
+console.log(year3);
