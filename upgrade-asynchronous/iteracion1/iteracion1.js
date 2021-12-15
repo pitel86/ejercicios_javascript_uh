d10612fb0d567a43949a2976350f35b8ae36e49e
pitel86 <pitelsergio@gmail.com>
2021-12-15 20:06:12 +0100

    Iteration#1 Completada

--- upgrade-asynchronous/iteracion1/iteracion1.js
+++ upgrade-asynchronous/iteracion1/iteracion1.js
@@ -1,11 +1,15 @@
 // 1.1 Utiliza esta url de la api Agify 'https://api.agify.io?name=michael' para 
 // hacer un .fetch() y recibir los datos que devuelve. Imprimelo mediante un 
 // console.log(). Para ello, es necesario que crees un .html y un .js.
+fetch("https://api.agify.io?name=michael")
+  .then((res) => res.json())
+  .then((myJson) => console.log(myJson));
 
 // 2.1 Dado el siguiente javascript y html. Añade la funcionalidad necesaria usando 
 // fetch() para hacer una consulta a la api cuando se haga click en el botón, 
 // pasando como parametro de la api, el valor del input.
-const baseUrl = 'https://api.nationalize.io'; 
+
+//EN iteracion1.2.1.js
 
 
 // 2.3 En base al ejercicio anterior. Crea dinamicamente un elemento  por cada petición 
@@ -13,6 +17,11 @@ const baseUrl = 'https://api.nationalize.io';
 // EJ: El nombre Pepe tiene un 22 porciento de ser de ET y un 6 porciento de ser 
 // de MZ.
 
+//En iteracion1.2.2.js
+
 // 2.4 En base al ejercicio anterior, crea un botón con el texto 'X' para cada uno 
 // de los p que hayas insertado y que si el usuario hace click en este botón 
 // eliminemos el parrafo asociado.
+
+
+//EN iteracion1.2.3.js
